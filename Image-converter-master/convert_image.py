d71e5a9547615f68ef96ea083024c88da6a46f3c
Abuzar Deshmukh <145523833+Abuzar6zbxa@users.noreply.github.com>
2024-02-12 23:11:25 +0530

    Add files via upload

---
+++ Image-converter-master/convert_image.py
@@ -0,0 +1,68 @@
+from PIL import Image
+from os import listdir
+from os.path import splitext
+import os
+import shutil
+
+
+def main(path, backup, format):
+    global target_directory
+    global back
+    global new_extension
+    target_directory = path
+    back = backup
+    new_extension = format
+    change_dir_to = os.chdir("{}".format(target_directory))
+    files(target_directory)
+
+
+def files(target_directory):
+    image_extension = ['.bmp', '.jpg', '.png', '.jpeg', '.gif']
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if 'BKP_OLD_IMAGES' not in root:
+                filename = os.path.join(root, name)
+                file, extension = splitext(filename)
+                dir = target_directory
+                if extension in image_extension:
+                    convert(file, extension, dir, new_extension, filename)
+
+
+def convert(file, extension, dir, new_extension, filename):
+    if extension != new_extension:
+        try:
+            print(os.path.join(dir, file + extension))
+            im = Image.open(os.path.join(dir, file + extension))
+            im.save(os.path.join(dir, file + new_extension))
+            print('converted image: {}'.format(filename))
+        except OSError as error:
+            print("It's not possible to convert the file {}.\nError code: {}".format(
+                filename, repr(error)))
+        if back == True:
+            do_backup(dir, file, extension)
+        elif back == False:
+            do_exclusion(dir, file, extension)
+
+
+def do_backup(dir, file, extension):
+    try:
+        if not os.path.isdir(os.path.join(dir, 'BKP_OLD_IMAGES')):
+            # creates the backup directory
+            os.mkdir(os.path.join(dir, 'BKP_OLD_IMAGES'))
+            print('Created the {} directory'.format('BKP_OLD_IMAGES'))
+    except:
+        print("It wasn't possible to create the dir")
+    try:
+        dest = os.path.join(dir, 'BKP_OLD_IMAGES', file + extension)
+        source = os.path.join(dir, file + extension)
+        shutil.move(source, dest)
+    except:
+        print("It wasn't possible to move")
+
+
+def do_exclusion(dir, file, extension):
+    source = os.path.join(dir, file + extension)
+    try:
+        os.remove(source)
+    except:
+        print("It wasn't possible to delete")
