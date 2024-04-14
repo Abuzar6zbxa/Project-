83895cfd3ff001f729eb44b490b6e16eba309c4d
Abuzar Deshmukh <145523833+Abuzar6zbxa@users.noreply.github.com>
2024-04-14 18:57:08 +0530

    Update pyhon_colour_game.py

--- Python-Tkinter-Color-Game-main/pyhon_colour_game.py
+++ Python-Tkinter-Color-Game-main/pyhon_colour_game.py
@@ -8,7 +8,7 @@ colours = ['Red','Blue','Green','Pink','Black',
 score = 0
   
 # the game time left, initially 30 seconds.
-timeleft = 30
+timeleft = 30 
   
 # function that will start the game.
 def startGame(event):
